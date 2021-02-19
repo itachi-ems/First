bf4866fef10eaf345f2e45a40cfeac5d1d684047
harsh <jshahharsh@gmail.com>
2021-02-19 12:03:07 +0530

    Adding 3rd line on feature3

--- demo3.java
+++ demo3.java
@@ -4,6 +4,7 @@ class demo3
  {
   System.out.println("This file was added directly from github");
   System.out.println("This file was added directly from github into feature1 branch");  
+  System.out.println("This is 3rd line from feature 3");
  }
   }
  
