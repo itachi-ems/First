3c2ecbfda0cbbe89cb13047eda13b5cd538ddc7a
harsh <jshahharsh@gmail.com>
2021-02-19 11:59:10 +0530

    Updating 1st line on demo3 on master

--- demo3.java
+++ demo3.java
@@ -2,7 +2,7 @@ class demo3
 {
   public static void main(String args[])
  {
-  System.out.println("This file was added directly from github");
+  System.out.println("This file was added directly from github on the master branch");
   System.out.println("This file was added directly from github into feature1 branch");  
 System.out.println("This is the third line");
  }
